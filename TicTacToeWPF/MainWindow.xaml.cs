8cd85fa303bbd6e78c04b5712c1f01c453c7f8c4
AlaskaJay <thatthinker@gmail.com>
2018-03-19 19:18:37 -0400

    Made it work

--- TicTacToeWPF/MainWindow.xaml.cs
+++ TicTacToeWPF/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace TicTacToeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        TicTacToe board;
+
         public MainWindow()
         {
             InitializeComponent();
+            board = new TicTacToe();
+        }
+
+        private void drawBoard()
+        {
+            butt00.Content = board.getSpace(0, 0);
+            butt01.Content = board.getSpace(0, 1);
+            butt02.Content = board.getSpace(0, 2);
+            butt10.Content = board.getSpace(1, 0);
+            butt11.Content = board.getSpace(1, 1);
+            butt12.Content = board.getSpace(1, 2);
+            butt20.Content = board.getSpace(2, 0);
+            butt21.Content = board.getSpace(2, 1);
+            butt22.Content = board.getSpace(2, 2);
+        }
+
+        private void calls(int x, int y)
+        {
+            board.takeTurn(x, y);
+            drawBoard();
+        }
+
+        private void butt00_Click(object sender, RoutedEventArgs e)
+        {
+            calls(0, 0);
+        }
+
+        private void butt01_Click(object sender, RoutedEventArgs e)
+        {
+            calls(0, 1);
+        }
+
+        private void butt02_Click(object sender, RoutedEventArgs e)
+        {
+            calls(0, 2);
+        }
+
+        private void butt10_Click(object sender, RoutedEventArgs e)
+        {
+            calls(1, 0);
+        }
+
+        private void butt11_Click(object sender, RoutedEventArgs e)
+        {
+            calls(1, 1);
+        }
+
+        private void butt12_Click(object sender, RoutedEventArgs e)
+        {
+            calls(1, 2);
+        }
+
+        private void butt20_Click(object sender, RoutedEventArgs e)
+        {
+            calls(2, 0);
+        }
+
+        private void butt21_Click(object sender, RoutedEventArgs e)
+        {
+            calls(2, 1);
+        }
+
+        private void butt22_Click(object sender, RoutedEventArgs e)
+        {
+            calls(2, 2);
         }
     }
 }
---
+++ TicTacToeWPF/TicTacToe.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeWPF
+{
+    class TicTacToe
+    {
+        private char DEFAULT = '*';
+        private char X = 'X';
+        private char O = 'O';
+
+        private char[,] board;
+        private bool win;
+        private int turn;
+
+        public TicTacToe()
+        {
+            board = new char[3,3];
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = DEFAULT;
+                }
+            }
+            win = false;
+            turn = 0;
+        }
+
+        public String getSpace(int x, int y)
+        {
+            if (win)
+            {
+                if (turn % 2 == 0)
+                    return "O wins!";
+                return "X wins!";
+            }
+            if (board[x, y] == DEFAULT)
+                return x + ", " + y;
+            return "" + board[x, y];
+        }
+
+        public void takeTurn(int x, int y)
+        {
+            if (!over())
+            {
+                if (turn % 2 == 0)
+                    takeTurn(x, y, X);
+                else
+                    takeTurn(x, y, O);
+
+                checkWin();
+            }
+            else
+                reset();
+
+        }
+
+        public void takeTurn(int x, int y, char player)
+        {
+            if(board[x, y] == DEFAULT)
+            {
+                board[x, y] = player;
+                turn++;
+            }
+            
+            
+        }
+
+        private bool over()
+        {
+            return win || turn >= 9;
+        }
+
+        private void reset()
+        {
+            board = new char[3, 3];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = DEFAULT;
+                }
+            }
+            win = false;
+            turn = 0;
+        }
+
+        private void checkWin()
+        {
+            if (board[1, 1] != DEFAULT)
+            {
+                if (board[0, 0] == board[1, 1] && board[2, 2] == board[1, 1])
+                    win = true;
+                if (board[2, 0] == board[1, 1] && board[0, 2] == board[1, 1])
+                    win = true;
+                if (board[1, 0] == board[1, 1] && board[1, 2] == board[1, 1])
+                    win = true;
+                if (board[0, 1] == board[1, 1] && board[2, 1] == board[1, 1])
+                    win = true;
+            }
+            if (board[0, 0] != DEFAULT)
+            {
+                if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2])
+                    win = true;
+                if (board[0, 0] == board[1, 0] && board[0, 0] == board[2, 0])
+                    win = true;
+            }
+            if (board[2, 2] != DEFAULT)
+            {
+                if (board[2, 2] == board[2, 1] && board[2, 2] == board[2, 0])
+                    win = true;
+                if (board[2, 2] == board[1, 2] && board[2, 2] == board[0, 2])
+                    win = true;
+            }
+        }
+    }  
+}
